f5af648f73c39b371ae238e06d920c4c05f54cc1
kampersanda <shnsk.knd@gmail.com>
2019-03-01 12:57:26 +0900

    add new file

---
+++ check_null.cpp
@@ -0,0 +1,31 @@
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+
+#include "cmdline.h"
+
+int main(int argc, char* argv[]) {
+  cmdline::parser p;
+  p.add<std::string>("input_fn", 'i', "input file name", true);
+  p.parse_check(argc, argv);
+
+  auto input_fn = p.get<std::string>("input_fn");
+
+  std::ifstream ifs(input_fn);
+  if (!ifs) {
+    std::cerr << "open error: " << input_fn << '\n';
+    return 1;
+  }
+
+  char c;
+  while (ifs.get(c)) {
+    if (c == '\0') {
+      std::cout << "NULL character is contained!!" << std::endl;
+      return 0;
+    }
+  }
+
+  std::cout << "NULL character is NOT contained" << std::endl;
+  return 0;
+}
